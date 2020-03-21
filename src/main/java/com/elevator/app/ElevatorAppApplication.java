91c34a1a155281ca9200a136bf401f1572297622
Vishnukanth Sivakumar <smvishnukanth@gmail.com>
2020-03-21 19:24:13 +1100

    project created - first commit

---
+++ src/main/java/com/elevator/app/ElevatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.elevator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorAppApplication.class, args);
+	}
+
+}
